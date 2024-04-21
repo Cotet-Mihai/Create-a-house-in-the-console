d0c29005db4b416fd8bad2eba95ff4d3c30562f2
Cotet Mihai <cotet.mihaita.cornel@gmail.com>
2024-04-21 14:48:04 +0300

    First Commit

---
+++ House/House.cpp
@@ -0,0 +1,207 @@
+﻿#include <iostream>
+#include "house.h"
+
+using namespace std;
+
+// Main Test
+void mainTest() {
+
+	// create some Houses
+	House h1(-5), h2(7, '^'), h3(12, 'W', 'o'), h4(50, '$', '-');
+	// display original Houses
+
+	cout << "h1 has size = " << h1.GetSize() << " units.\n";
+	h1.Draw();
+	cout << "\nh2 has size = " << h2.GetSize() << " units.\n";
+	h2.Draw();
+	cout << "\nh3 has size = " << h3.GetSize() << " units.\n";
+	h3.Draw();
+	cout << "\nh4 has size = " << h4.GetSize() << " units.\n";
+	h4.Draw();
+	cout << '\n';
+
+	h1.Shrink(); // demonstrate shrink
+	h2.Shrink();
+	h3.Grow(); // and grow
+	h4.Grow();
+	cout << "h1 now has size = " << h1.GetSize() << " units.\n";
+	cout << "h2 now has size = " << h2.GetSize() << " units.\n";
+	cout << "h3 now has size = " << h3.GetSize() << " units.\n";
+	cout << "h4 now has size = " << h4.GetSize() << " units.\n";
+
+	// demonstrate perimeter
+	cout << "h2 has perimeter = " << h2.Perimeter() << " units.\n";
+	cout << "h3 has perimeter = " << h3.Perimeter() << " units.\n";
+	// and area
+	cout << "h2 has area = " << h2.Area() << " square units.\n\n";
+	cout << "h3 has area = " << h3.Area() << " square units.\n\n";
+
+	h1.Draw();
+	h1.Grow();               // show that fill character
+	cout << "h1 grows:\n";   // appears only when size
+	h1.Draw();               // is at least 3
+	h1.Grow();
+	cout << "... and grows:\n";
+	h1.Draw();
+	cout << '\n';
+
+	h1 = h2; // demonstrate the default overload of the
+	// assignment operator
+	cout << "h1 now has size = " << h1.GetSize() << " units.\n";
+	h1.Draw();
+
+	// demonstrate the changing of border and fill characters
+	h2.SetBorder('@');
+	h2.SetFill('-');
+	cout << "h2 now looks like:\n";
+	h2.Draw();
+	cout << '\n';
+	h2.SetBorder('\n');	 // illegal border
+	h2.SetFill('\a');	 // illegal fill
+	cout << "h2 now looks like:\n";
+	h2.Draw();
+	cout << '\n';
+
+	cout << "\nHere is a summary on h3:\n"; // demonstrate summary
+	h3.Summary();
+}
+
+// My Test
+void myTest() {
+
+	int size; // Declare a variable to store the size of the house
+	do {
+		cout << "Enter the size of the house (a positive integer): "; // Prompt the user to enter the size of the house
+		cin >> size; // Read the input for the size of the house
+
+		if (size <= 0) { // Check if the input is not a positive integer
+			cout << "Invalid input. Please enter a positive integer." << endl << endl; // Display an error message for invalid input
+			cin.clear(); // Clear the error state of the input stream
+			cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore the remaining characters until the next newline
+		}
+	} while (size <= 0); // Repeat the loop if the input is not a positive integer
+
+
+	House house(size); // Create a House object with the specified size
+	house.Draw(); // Draw the initial house
+
+	cout << endl;
+	house.Summary(); // Display the summary of the initial house
+	cout << endl;
+
+	bool isRunning = true; // Initialize the flag to control the loop
+
+	while (isRunning) { // Start a loop for modifying the house
+		cout << "|------------------------------|" << endl;
+		cout << "| Menu for modifying the house |" << endl;
+		cout << "|------------------------------|" << endl;
+		cout << "|1| Increase House Size        |" << endl; // Option to increase house size
+		cout << "|2| Decrease House Size        |" << endl; // Option to decrease house size
+		cout << "|3| Change Border              |" << endl; // Option to change the border of the house
+		cout << "|4| Change Interior            |" << endl; // Option to change the interior of the house
+		cout << "|------------------------------|" << endl;
+		cout << "|9| Close the application      |" << endl; // Option to close the application
+		cout << "|------------------------------|" << endl;
+		cout << endl;
+		cout << "Navigate: ";
+
+		int choice;
+
+		cin >> choice; // Read the user's choice
+		cout << endl;
+
+		switch (choice) // Perform actions based on the user's choice
+		{
+		case (1):
+			house.Grow(); // Increase the size of the house
+			house.Draw(); // Redraw the house
+
+			cout << endl;
+
+			house.Summary(); // Display the summary of the modified house
+
+			cout << endl;
+			break;
+
+		case (2):
+			house.Shrink(); // Decrease the size of the house
+			house.Draw(); // Redraw the house
+
+			cout << endl;
+
+			house.Summary(); // Display the summary of the modified house
+
+			cout << endl;
+			break;
+
+		case (3):
+
+			cout << "Symbol to change the border: ";
+			char changeBorder;
+			cin >> changeBorder;
+
+			house.SetBorder(changeBorder); // Change the border symbol of the house
+			house.Draw(); // Redraw the house
+
+			cout << endl;
+
+			house.Summary(); // Display the summary of the modified house
+
+			cout << endl;
+			break;
+
+		case (4):
+			cout << "Symbol to change the interior: ";
+			char changeFill;
+			cin >> changeFill;
+
+			house.SetFill(changeFill); // Change the interior symbol of the house
+			house.Draw(); // Redraw the house
+
+			cout << endl;
+
+			house.Summary(); // Display the summary of the modified house
+
+			cout << endl;
+			break;
+
+		case (9):
+			cout << "Are you sure you want to close the application? (y): "; // Ask the user if they want to close the application
+			char answer; // Declare a variable to store the user's response
+			cin >> answer; // Read the user's response
+			cout << endl << endl;
+
+			answer = tolower(answer); // Convert the user's response to lowercase
+
+			if (answer == 'y') { // Check if the user's response is 'y'
+				isRunning = false; // Set the variable isRunning to false to exit the loop
+			}
+			else {
+				cout << "Continuing the application." << endl; // Display a message indicating that the application will continue
+			}
+
+			cout << endl;
+			break;
+
+
+		default:
+			cout << "Command not recognized. Please enter a number between 1 - 4 or 9 to close the application!";
+
+			//For the case that the user enters anything other than an integer
+
+			cin.clear(); // Clear the error state of the input stream
+			cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore the remaining characters until the next newline
+			break;
+		}
+	}
+}
+
+
+
+int main()
+{
+	myTest();
+		
+	return 0;
+}
+
---
+++ House/house.h
@@ -0,0 +1,107 @@
+#include <iostream>
+
+using namespace std;
+
+class House {
+private:
+    int _base; // the base size of the house
+    string _border; // the border character
+    string _fill; // the fill character
+
+public:
+    // Constructor
+    House(int base, char border = 'X', char fill = '*') {
+
+        
+        _base = (base <= 3) ? 3 : (base >= 30) ? 30 : base; // Ensure base size is within range 3, 30
+        _border = border; // Initialize border character
+        _fill = fill; // Initialize fill character
+    }
+
+    // Get the base size of the house
+    int GetSize() { return _base; }
+
+    // Calculate and return the perimeter of the house
+    int Perimeter() { return (3 * (_base + 2) + (2 * _base)); }
+
+    // Calculate and return the area of the house
+    float Area() { return (pow((_base + 2), 2) * (sqrt(3) / 4)) + (pow(_base, 2)); }
+
+    // Increase the base size of the house by 1, unless it exceeds 30
+    void Grow() { _base < 30 ? _base++ : _base; }
+
+    // Decrease the base size of the house by 1, unless it goes below 3
+    void Shrink() { _base > 3 ? _base-- : _base; }
+
+    // Set the border character of the house
+    void SetBorder(char newBorder) {
+        // Limit newBoarder to characters between ASCII 33 and ASCII 126
+        newBorder >= '!' && newBorder <= '~' ? _border = newBorder : _border = 'X';
+    }
+
+    // Set the fill character of the house
+    void SetFill(char newFill) {
+        // Limit newFill to characters between ASCII 33 and ASCII 126
+        newFill >= '!' && newFill <= '~' ? _fill = newFill : _fill = '*';
+    }
+
+    // Draw the house
+    void Draw() const {
+        // Roof
+        for (int i = 0; i <= _base; i++) {
+            // Distance from the edge
+            for (int j = 0; j < _base + 1 - i; j++) { cout << " "; }
+            // Display the first outline in a row
+            cout << _border << ' ';
+            // If it is the first row, go directly to the next row
+            if (i == 0) {
+                cout << endl;
+                continue;
+            }
+            // If it is the second row, add the second border
+            else if (i == 1) {
+                cout << _border << ' ' << endl;
+                continue;
+            }
+            else {
+                // Fill the row with the fill character
+                for (int j = 0; j < i - 1; j++) { cout << _fill << ' '; }
+                // Display the last border in the row
+                cout << _border << ' ';
+            }
+            // Move to the next line after each row
+            cout << endl;
+        }
+
+        // The base of the roof
+        cout << _border << ' ' << _border << ' '; // Display the border twice
+        for (int i = 0; i < _base - 2; i++) { cout << _fill << ' '; } // Fill the base
+        cout << _border << ' ' << _border << ' ' << endl; // Display the border twice and move to the next line
+
+        // Body
+        for (int i = 0; i < _base - 1; i++) {
+            // Display the border and fill the body
+            cout << "  " << _border << ' ';
+            for (int j = 0; j < _base - 2; j++) {
+                // If it is the last row, fill with the border character
+                cout << (i == _base - 2 ? _border : _fill) << ' ';
+            }
+            // Display the last border and move to the next line
+            cout << _border << ' ' << endl;
+        }
+    }
+
+    // Overload the << operator to display the house
+    friend ostream& operator<<(ostream& os, const House& house) {
+        house.Draw();
+        return os;
+    }
+
+    // Display a summary of the house
+    void Summary() {
+        cout << "Summary of the house" << endl << endl;
+        cout << "Size: " << GetSize() << endl;
+        cout << "Perimeter: " << Perimeter() << endl;
+        cout << "Area: " << Area() << endl;
+    }
+};
